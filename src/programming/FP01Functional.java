cd36c3d59d19cb3c979e2ed86108768073fad83d
Yoshiki <b1700336@gl.aiu.ac.jp>
2024-09-13 05:12:01 +0900

    Improved a Java functional program with filter

--- src/programming/FP01Functional.java
+++ src/programming/FP01Functional.java
@@ -6,18 +6,27 @@ public class FP01Functional {
 	
 	public static void main(String[] args) {
 		
-		printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+//		printAllNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
+		printEvenNumbersInListFunctional(List.of(12,9,13,4,6,2,4,12,15));
 		
 	}
-	
-	private static void print(int number) {
-		System.out.println(number);
+
+	private static boolean isEven(int number) {
+		return number % 2 == 0;
 	}
 
 	private static void printAllNumbersInListFunctional(List<Integer> numbers) {
 		
 		numbers.stream()
-			.forEach(FP01Functional::print);
+			.forEach(System.out::println);
+		
+	}
+	
+	private static void printEvenNumbersInListFunctional(List<Integer> numbers) {
+		
+		numbers.stream()
+		.filter(FP01Functional::isEven)
+		.forEach(System.out::println);;
 		
 	}
 
